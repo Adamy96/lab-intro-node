2ca52d6a23e2d5b9b7a31b0dd526d886bf12540a
Adamy <viniciusbohreradamy@gmail.com>
2019-09-24 00:08:18 -0300

    Lab completed!

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items = [];
+      this.length = this.items.length;
+    }
+
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a - b);
+      this.length += 1;
+    }
+    get(pos) {
+      if (pos <= this.length) {
+        return this.items[pos];
+      } else {
+        throw new Error('OutOfBounds');
+      }
+    }
+    max() {
+      if (this.length > 0) {
+        return Math.max(...this.items);
+      } else {
+        throw new Error("EmptySortedList");
+      }
+    }
+    min() {
+      if (this.length > 0) {
+        return Math.min(...this.items);
+      } else {
+        throw new Error('EmptySortedList');
+      }
+    }
+    average() {
+      if (this.length > 0) {
+        return this.sum() / this.length;
+      } else {
+        throw new Error('EmptySortedList');
+      }
+    }
+    sum() {
+      if (this.length > 0) {
+        return this.items.reduce((acc, e) => e + acc, 0);
+      } else {
+        return 0;
+      }      
+    }
   };
   
   module.exports = SortedList;
